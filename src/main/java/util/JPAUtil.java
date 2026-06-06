995449b07c0426b4fe7ad348ad8ba26702b647fc
vitoria <vitoriacristine.macedo@gmail.com>
2026-06-06 17:23:47 -0400

    Configurando variaveis de ambiente

--- src/main/java/util/JPAUtil.java
+++ src/main/java/util/JPAUtil.java
@@ -4,10 +4,22 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JPAUtil {
 
-    private static final EntityManagerFactory factory =
-            Persistence.createEntityManagerFactory("crudPU");
+    private static final EntityManagerFactory factory;
+
+    static {
+        Map<String, String> config = new HashMap<>();
+
+        config.put("jakarta.persistence.jdbc.url", System.getenv("DB_URL"));
+        config.put("jakarta.persistence.jdbc.user", System.getenv("DB_USER"));
+        config.put("jakarta.persistence.jdbc.password", System.getenv("DB_PASSWORD"));
+
+        factory = Persistence.createEntityManagerFactory("crudPU", config);
+    }
 
     public static EntityManager getEntityManager() {
         return factory.createEntityManager();
